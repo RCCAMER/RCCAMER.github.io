787d25c9924614c0c903cf79dfd955e89ac71f99
RCCAMER <rcameron@sycamoreleaf.com>
2015-10-02 16:55:18 -0500

    add print

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<!DOCTYPE html>
-<html>
-<body>
-   <p>Be Extremely Careful when clicking these links... they take you to an amazing and wonderful place that has been created by the One and Only Ryan Cameron :) *inspired by Sam Freund* :</p>
-   <p><a href="Stolen_Item_Owners-3/home.php">home</a></p>
-   <p><a href="Stolen_Item_Owners-3/profile.php">profile</a></p>
-   <p><a href="Stolen_Item_Owners-3/missingitems.php">missing items</a></p>
-   <p><a href="Stolen_Item_Owners-3/nonstolenitemregister.php">Item register</a></p>
-</body>
-</html>
+<?
+print"(<html>");
+print("<body>");
+   print("<p>Be Extremely Careful when clicking these links... they take you to an amazing and wonderful place that has been created by the One and Only Ryan Cameron :) *inspired by Sam Freund* :</p>");
+   print("<p><a href="Stolen_Item_Owners-3/home.php">home</a></p>");
+   print("<p><a href="Stolen_Item_Owners-3/profile.php">profile</a></p>");
+   print("<p><a href="Stolen_Item_Owners-3/missingitems.php">missing items</a></p>");
+   print("<p><a href="Stolen_Item_Owners-3/nonstolenitemregister.php">Item register</a></p>");
+prinT("</body>");
+print("</html>");
+?>
