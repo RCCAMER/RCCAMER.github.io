0357d194785efbf97587f69c10452fb5aba022b8
RCCAMER <rcameron@sycamoreleaf.com>
2015-12-04 14:46:55 -0600

    first commit for this

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?
 print"(<html>");
 print("<body>");
-   print("<p>Be Extremely Careful when clicking these links... they take you to an amazing and wonderful place that has been created by the One and Only Ryan Cameron :) *inspired by Sam Freund* :</p>");
-   print("<p><a href="Stolen_Item_Owners-3/home.php">home</a></p>");
-   print("<p><a href="Stolen_Item_Owners-3/profile.php">profile</a></p>");
-   print("<p><a href="Stolen_Item_Owners-3/missingitems.php">missing items</a></p>");
-   print("<p><a href="Stolen_Item_Owners-3/nonstolenitemregister.php">Item register</a></p>");
+   print("<p>Vaping Knowledge</p>");
+   print("<p><a href="vapeknowledge/home.php">home</a></p>");
 prinT("</body>");
 print("</html>");
 ?>
